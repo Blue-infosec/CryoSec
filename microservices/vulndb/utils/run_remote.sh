4072d1e185d0b083a05c73a132ed2228f994ddc2
DevManTillis <mtillis@tillisautomation.com>
2019-09-04 15:01:44 -0400

    Add stack utility to modify stack state

--- microservices/vulndb/utils/run_remote.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#json=$(cat sam-app/events/event.json)
-json=$(cat event.json.new)
-curl -X GET -d @event.json.new -H "Content-Type: application/json" https://omdul8jp29.execute-api.us-west-2.amazonaws.com/default/vulndb
-
-#curl -X GET -d @event.json.new \
-#  -H "Content-Type: application/json" \
-#  https://omdul8jp29.execute-api.us-west-2.amazonaws.com/default/vulndb
