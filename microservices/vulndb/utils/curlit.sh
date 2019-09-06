0e82f2a4ba33f25406e76e75f17072e802393dbe
DevManTillis <mtillis@tillisautomation.com>
2019-09-06 12:56:13 -0400

    Enable write to dynamodb from AWS console

--- microservices/vulndb/utils/curlit.sh
+++ microservices/vulndb/utils/curlit.sh
@@ -1,3 +1,7 @@
-event=$(cat event.get.json)
-curl -X GET -H 'Content-Type: application/json' \
-  "https://omdul8jp29.execute-api.us-west-2.amazonaws.com/default/vulndb"
+#event=$(cat event.get.json)
+#curl -X GET -H 'Content-Type: application/json' \
+#  "https://omdul8jp29.execute-api.us-west-2.amazonaws.com/default/vulndb"
+event=$(cat POST_event.json)
+curl -d "$event" -H "Content-Type: application/json" \
+  -X POST https://omdul8jp29.execute-api.us-west-2.amazonaws.com/default/vulndb
+
