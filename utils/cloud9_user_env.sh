aab8c731323bbb579a998bae665218d4e1ecdd78
EC2 Default User <ec2-user@ip-172-31-22-69.us-west-2.compute.internal>
2019-10-24 12:14:42 +0000

    Add cloud9 user env update script

--- utils/cloud9_user_env.sh
+++ utils/cloud9_user_env.sh
@@ -3,3 +3,10 @@
 virtualenv cryosec_venv
 . cryosec_venv/bin/activate
 pip install 'ansible==2.8'
+cat <<EOT >> /home/ec2-user/.bashrc
+. /home/ec2-user/environment/cryosec_venv/bin/activate
+cd /home/ec2-user/environment/CryoSec
+echo "Updating CryoSec Repo..."
+git pull
+PATH=$PATH:/home/ec2-user/environment/CryoSec
+EOT
